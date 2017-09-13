366bcbe12b4f473b9165c6054c9e5dcf820782ed
Ronald Goedeke <ronald.goedeke@outlook.com>
2017-09-13 21:22:22 +0200

    Added in the entity rendering

--- src/main/java/com/bubble/gunmod/proxy/ClientProxy.java
+++ src/main/java/com/bubble/gunmod/proxy/ClientProxy.java
@@ -1,7 +1,19 @@
 package com.bubble.gunmod.proxy;
 
 import com.bubble.gunmod.client.KeyBindings;
+import com.bubble.gunmod.client.render.RenderAdvancedBullet;
+import com.bubble.gunmod.client.render.RenderSimpleBullet;
+import com.bubble.gunmod.client.render.RenderSimpleRifleAmmo;
+import com.bubble.gunmod.client.render.RenderSimpleShotgunAmmo;
+import com.bubble.gunmod.common.entity.EntityAdvancedBullet;
+import com.bubble.gunmod.common.entity.EntitySimpleBullet;
+import com.bubble.gunmod.common.entity.EntitySimpleRifleAmmo;
+import com.bubble.gunmod.common.entity.EntitySimpleShotgunAmmo;
 
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
@@ -24,35 +36,34 @@ public class ClientProxy extends CommonProxy {
 	}
 
 	public void registerEntityModels() {
-		// TODO @BubbleTrouble add the Render classes!
-		// RenderingRegistry.registerEntityRenderingHandler(EntityAdvancedBullet.class,
-		// new IRenderFactory<EntityAdvancedBullet>() {
-		// @Override
-		// public Render<EntityAdvancedBullet> createRenderFor(RenderManager manager) {
-		// return new RenderAdvancedBullet(manager);
-		// }
-		// });
-		// RenderingRegistry.registerEntityRenderingHandler(EntitySimpleBullet.class,
-		// new IRenderFactory<EntitySimpleBullet>() {
-		// @Override
-		// public Render<EntitySimpleBullet> createRenderFor(RenderManager manager) {
-		// return new RenderSimpleBullet(manager);
-		// }
-		// });
-		// RenderingRegistry.registerEntityRenderingHandler(EntitySimpleRifleAmmo.class,
-		// new IRenderFactory<EntitySimpleRifleAmmo>() {
-		// @Override
-		// public Render<EntitySimpleRifleAmmo> createRenderFor(RenderManager manager) {
-		// return new RenderSimpleRifleAmmo(manager);
-		// }
-		// });
-		// RenderingRegistry.registerEntityRenderingHandler(EntitySimpleShotgunAmmo.class,
-		// new IRenderFactory<EntitySimpleShotgunAmmo>() {
-		// @Override
-		// public Render<EntitySimpleShotgunAmmo> createRenderFor(RenderManager manager)
-		// {
-		// return new RenderSimpleShotgunAmmo(manager);
-		// }
-		// });
+		 RenderingRegistry.registerEntityRenderingHandler(EntityAdvancedBullet.class,
+		 new IRenderFactory<EntityAdvancedBullet>() {
+		 @Override
+		 public Render<EntityAdvancedBullet> createRenderFor(RenderManager manager) {
+		 return new RenderAdvancedBullet(manager);
+		 }
+		 });
+		 RenderingRegistry.registerEntityRenderingHandler(EntitySimpleBullet.class,
+		 new IRenderFactory<EntitySimpleBullet>() {
+		 @Override
+		 public Render<EntitySimpleBullet> createRenderFor(RenderManager manager) {
+		 return new RenderSimpleBullet(manager);
+		 }
+		 });
+		 RenderingRegistry.registerEntityRenderingHandler(EntitySimpleRifleAmmo.class,
+		 new IRenderFactory<EntitySimpleRifleAmmo>() {
+		 @Override
+		 public Render<EntitySimpleRifleAmmo> createRenderFor(RenderManager manager) {
+		 return new RenderSimpleRifleAmmo(manager);
+		 }
+		 });
+		 RenderingRegistry.registerEntityRenderingHandler(EntitySimpleShotgunAmmo.class,
+		 new IRenderFactory<EntitySimpleShotgunAmmo>() {
+		 @Override
+		 public Render<EntitySimpleShotgunAmmo> createRenderFor(RenderManager manager)
+		 {
+		 return new RenderSimpleShotgunAmmo(manager);
+		 }
+		 });
 	}
 }
